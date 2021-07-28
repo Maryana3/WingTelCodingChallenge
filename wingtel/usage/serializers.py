32dd917b4054d6ef6852b1d81924a9ec299804ad
mac <mac@gmail.com>
2021-07-28 18:28:09 +0300

    update business logic for UsageMetricsBySubscriptionUsageTypeViewSet and SubscriptionsExceedingUsagePriceLimitViewSet

--- wingtel/usage/serializers.py
+++ wingtel/usage/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 import decimal
 from wingtel.usage.models import UsageRecord
+from django.db.models import Count, Sum, Case, When, IntegerField
 
 
 class SubscriptionsPriceLimitSerializer(serializers.ModelSerializer):
@@ -11,14 +12,19 @@ class SubscriptionsPriceLimitSerializer(serializers.ModelSerializer):
         try:
             price_limit = float(self.context['request'].query_params.get("price_limit", None))
             price_limit = decimal.Decimal(price_limit)
+            exceeded_price_subscriptions = obj.subscriptions.values('id').annotate(plan_price=Sum('plan__price'),
+                                                                    price_exceeded=Sum('plan__price')-price_limit).\
+                    order_by('id')
+
+            """
             subscriptions = obj.subscriptions.all()
-            print("subscriptions ",subscriptions)
             for subscription in subscriptions:
                 if price_limit and subscription.plan.price:
                     exceeded_price_subscriptions.append({"subscription_id": subscription.id,
                                                          "plan_price": subscription.plan.price,
                                                          "price_exceeded": subscription.plan.price
                                                                            - price_limit})
+            """
         except:
             pass
 
--- wingtel/usage/views.py
+++ wingtel/usage/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
 from wingtel.usage.models import UsageRecord
 from wingtel.usage.serializers import SubscriptionsPriceLimitSerializer
+from wingtel.subscriptions.serializers import Subscription
 from django.db.models import Q
 from rest_framework.response import Response
 from django.db.models import Count, Sum, Case, When, IntegerField
-from django.db import transaction
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -14,62 +14,22 @@ class UsageMetricsBySubscriptionUsageTypeViewSet(viewsets.ViewSet):
     """
 
     def list(self, request):
-        queryset = UsageRecord.objects.all()
-        subscription_id = self.request.query_params.get("subscription_id", None)
         start_date = self.request.query_params.get("start_date", None)
         end_date = self.request.query_params.get("end_date", None)
         usage_type = self.request.query_params.get("usage_type", None)
+        queryset = UsageRecord.objects.all()
         if usage_type:
-            print(queryset.count())
             queryset = queryset.filter(usage_type=usage_type)
-            print(queryset.count())
-        if subscription_id:
-            print(queryset.count())
-            queryset = queryset.filter(subscriptions__id=subscription_id)
-            print(queryset.count())
-        if start_date and end_date:
-            print(queryset.count())
+        if start_date or end_date:
             queryset = queryset.filter(
-                usage_date__range=[start_date, end_date])
-            print(queryset.count())
-
-        att_subscription__count = queryset.filter(
-            subscriptions__subscription_type="att").count()
-        print("att_subscription__count ",att_subscription__count)
-        sprint_subscription__count = queryset.filter(
-            subscriptions__subscription_type="sprint").count()
-        print("sprint_subscription__count ", sprint_subscription__count)
-        att_subscription__plan__price_sum = queryset.filter(
-            subscriptions__subscription_type="att",
+                usage_date__range=[start_date, end_date],
+            )
+        result = queryset.values(
+            'subscriptions__id'
         ).annotate(
-            subscriptions__plan__price=Sum("subscriptions__plan__price"),
-        )
-        print("att_subscription__plan__price_sum ",att_subscription__plan__price_sum)
-        sprint_subscription__plan__price_sum = queryset.filter(
-            subscriptions__subscription_type="sprint",
-        ).annotate(
-            subscriptions__plan__price=Sum("subscriptions__plan__price"),
-        )
-        print("sprint_subscription__plan__price_sum ",sprint_subscription__plan__price_sum)
-        """
-        result = queryset.aggregate(subscription_count=Count("subscription__id"),
-                                    total_price_usage=Sum("subscription__plan__price"),
-                                    att_subscription__count=Count(Case(When(subscriptions__subscription_type="att",
-                                                                                then=1),
-                                                                           output_field=IntegerField())),
-                                    sprint_subscription__count=Sum(
-                                        Case(When(subscriptions__subscription_type="sprint",
-                                                  then=subscriptions___plan__price),
-                                             output_field=IntegerField()),
-                                    att_subscription__plan__price_sum=Sum(Case(When(subscriptions__subscription_type="att",
-                                                                            then=subscriptions___plan__price),
-                                                                       output_field=IntegerField())),
-                                    sprint_subscription__plan__price_sum=Sum(Case(When(subscriptions__subscription_type="sprint",
-                                                                            then=subscriptions___plan__price),
-                                                                       output_field=IntegerField()))))
-        """
-        result["subscription_id"] = [subscription_id] if subscription_id else []
-
+            total_sum=Sum('price'),
+            total_usage=Count('price'),
+        ).order_by('subscriptions__id')
         return Response(result)
 
 
@@ -80,64 +40,7 @@ class SubscriptionsExceedingUsagePriceLimitViewSet(viewsets.ModelViewSet):
     queryset = UsageRecord.objects.all()
     serializer_class = SubscriptionsPriceLimitSerializer
 
-    @transaction.atomic
     def get_queryset(self):
 
         price_limit = self.request.query_params.get("price_limit", 0)
         return self.queryset.filter(subscriptions__plan__price__gt=price_limit)
-        """
-
-
-        from django.db import transaction
-        from datetime import datetime
-        from django.test import TestCase
-        from wingtel.subscriptions.models import Subscription
-        from wingtel.plans.models import Plan
-        from django.contrib.auth.models import User
-        from wingtel.usage.models import UsageRecord
-
-        print("User.objects ",User.objects.all().count())
-        user1 = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-        tmp1 = Plan.objects.create(name="tmp1",price=5)
-        tmp2 = Plan.objects.create(name="tmp2", price=10)
-        tmp3 = Plan.objects.create(name="tmp3", price=15)
-        tmp4 = Plan.objects.create(name="tmp4", price=20)
-        from decimal import Decimal
-
-        Subscription.objects.create(user=user1,plan=tmp4, status="new",subscription_type="att",ONE_KILOBYTE_PRICE = Decimal('0.001'),ONE_SECOND_PRICE = Decimal('0.001'))
-        Subscription.objects.create(user=user1,plan=tmp3, status="active",subscription_type="sprint")
-        Subscription.objects.create(user=user1,plan=tmp2, status="expired",subscription_type="att",ONE_KILOBYTE_PRICE = Decimal('0.001'),ONE_SECOND_PRICE = Decimal('0.001'))
-        Subscription.objects.create(user=user1,plan=tmp1, status="active",subscription_type="sprint")
-
-        import json
-        # Opening JSON file
-        f = open('wingtel/usage/fixtures.json',"rb" )
-        data = json.load(f)
-        try:
-            for item in data:
-                item_ = item['fields']
-                import pytz
-                if item["model"] == "usage.datausagerecord":
-                    obj = UsageRecord.objects.create(
-                        price=item_["price"],
-                        usage_date= datetime.strptime(item_["usage_date"],"%Y-%m-%dT%H:%M:%S.%fZ"),
-                        usage_type="data",
-                        used_metric_value=item_["kilobytes_used"]
-                    )
-                    print("obj",obj.subscriptions)
-                    obj.subscriptions.add(Subscription.objects.get(id=item_["subscription"]))
-                    obj.save()
-                if item["model"] == "usage.voiceusagerecord":
-                    obj = UsageRecord.objects.create(
-                        price=item_["price"],
-                        usage_date=datetime.strptime(item_["usage_date"],"%Y-%m-%dT%H:%M:%S.%fZ"),
-                        usage_type="voice",
-                        used_metric_value=item_["seconds_used"]
-                    )
-                    obj.subscriptions.add(Subscription.objects.get(id=item_["subscription"]))
-                    obj.save()
-        except Exception as e:
-            print("Exception ",e)
-        """
-
-
